fc20b819daac9af1647a419b4d2482e9293eb72e
Chris Pennington <chris@codinginpublic.dev>
2024-10-26 20:24:34 -0600

    finished code

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
 const food = defineCollection({
-  type: "content",
   schema: z.object({
     id: z.number(),
     title: z.string(),
@@ -11,6 +10,46 @@ const food = defineCollection({
   }),
 });
 
+const apiFood = defineCollection({
+  loader: async () => {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/filter.php?a=British"
+    );
+    const data = await response.json();
+    const foodData = await Promise.all(
+      data.meals.map(async (meal: any) => {
+        const mealRes = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`
+        );
+        const mealData = await mealRes.json();
+        return mealData.meals[0];
+      })
+    );
+
+    return foodData.map((meal) => ({
+      id: meal.idMeal,
+      ...meal,
+    }));
+  },
+  schema: z
+    .object({
+      // api structure
+      idMeal: z.string(),
+      strMeal: z.string(),
+      strInstructions: z.string(),
+      strMealThumb: z.string().url(),
+      strTags: z.string().or(z.null()),
+    })
+    .transform((data) => ({
+      id: Number(data.idMeal),
+      title: data.strMeal,
+      instructions: data.strInstructions,
+      thumbnail: data.strMealThumb,
+      tags: data.strTags ? data.strTags.split(",") : undefined,
+    })),
+});
+
 export const collections = {
   food,
+  apiFood,
 };
